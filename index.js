29a598f7588e3db6e381c3ab926ed7273a817768
Amit Kumar Madhoriya <amit.madhoriya@sage.com>
2024-10-01 15:47:34 +0100

    update pmd version to latest available

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ try {
 }
 
 function installPMD(){
-  var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.19.0/pmd-bin-6.19.0.zip -P /tmp'
+  var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F7.6.0/pmd-dist-7.6.0-bin.zip -P /tmp'
   var unzip = 'unzip /tmp/pmd-bin-6.19.0.zip -d /tmp'
   var mk = 'mkdir $HOME/pmd'
   var mv = 'mv /tmp/pmd-bin-6.19.0/* $HOME/pmd'
